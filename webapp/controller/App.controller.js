07f11ea46ff78e134e7419ae7e771eb625142b72
hogarehalli srinivas <srinivas.ramesha@gmail.com>
2018-05-23 14:08:48 +0000

    filters and sorter added

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define(["sap/ui/core/mvc/Controller",
 		"sap/m/MessageToast",
-		"mine/model/formatter"
+		"mine/model/formatter",
+		"sap/ui/model/Filter",
+		"sap/ui/model/FilterOperator"
 	],
-	function(Controller, MessageToast, formatter) {
+	function(Controller, MessageToast, formatter, Filter, FilterOperator) {
 		Controller.extend("mine.controller.App", {
 			formatter: formatter,
 			onPress: function() {
@@ -11,6 +13,30 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 				var sName = this.getView().getModel("helloPan").getProperty("/reciepient/name");
 				var sMsg = oBun.getText("MSG", [sName]);
 				MessageToast.show(sMsg);
+			},
+			onSearch: function(oEvent) {
+				var oLis = this.getView().byId("idList").getBinding("items"),
+					oTxt = oEvent.getParameter("query"),
+					aFilter = [];
+				var oFilt = new Filter({
+					path: "Category",
+					operator: FilterOperator.Contains,
+					value1: oTxt
+				});
+				aFilter.push(oFilt);
+				oLis.filter(aFilter);
+			},
+			onLiveChange: function(oEvent) {
+				var oVal = oEvent.getParameter("newValue"),
+					oList = this.getView().byId("idList").getBinding("items"),
+					bFilter = [];
+
+				bFilter.push(new Filter(
+					"ProductID",
+					FilterOperator.Contains,
+					oVal
+				));
+				oList.filter(bFilter);
 			}
 		});
 	});
